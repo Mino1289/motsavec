ca66da6ba6fce41646c8ee9e0df143bf6b57e7fc
Mino1289 <kretzvictor@orange.fr>
2022-07-20 18:22:01 +0200

    using axios and the api

--- src/components/FindWord.vue
+++ src/components/FindWord.vue
@@ -61,133 +61,7 @@
 </template>
 
 <script>
-import lesmots from "../assets/data.json";
-
-/**
- * Count the number of occurence of s in the string
- * @param {String} s
- * @returns
- */
-String.prototype.count = function (s) {
-  var result = 0;
-  for (let i = 0; i < this.length; i++) {
-    if (this[i] == s) result++;
-  }
-  return result;
-};
-
-/**
- *
- * @param {String[]} L la liste de lettre à tester
- * @param {String} m le mot à tester
- * @returns
- */
-const fil = (L, m) => {
-  var f = [];
-  for (let i = 0; i < L.length; i++) {
-    if (
-      m.includes(L[i]) &&
-      m.count(L[i]) >= L.filter((x) => x == L[i]).length
-    ) {
-      f.push(L[i]);
-    }
-  }
-  if (f.length == L.length) return true;
-  else return false;
-};
-
-/**
- *
- * @param {String[]} L la liste de lettre à tester
- * @param {String} m le mot à tester
- * @returns
- */
-const nofil = (L, m) => {
-  var f = [];
-  for (let i = 0; i < L.length; i++) {
-    if (m.includes(L[i])) {
-      f.push(L[i]);
-    }
-  }
-  if (f.length == 0) return true;
-  else return false;
-};
-
-/**
- * @param {String[]} L Liste de lettre qui ne doivent pas etre dans le mots.
- * @param {String[]} M Les mots à filtrer
- */
-const nocontain = (L, M) => {
-  var E = [];
-  M.forEach((mot) => {
-    if (nofil(L, mot)) {
-      E.push(mot);
-    }
-  });
-  return E;
-};
-
-/**
- * @param {String[]} L Les lettres
- * @param {String[]} M Les mots à filtrer
- */
-const contain = (L, M) => {
-  var E = [];
-  M.forEach((mot) => {
-    if (fil(L, mot)) {
-      E.push(mot);
-    }
-  });
-  return E;
-};
-
-/**
- * @param {String} s le mot commence par s
- * @param {String[]} Mots Liste des mots a filtrer
- */
-const start = (s, M) => {
-  var E = [];
-  M.forEach((mot) => {
-    if (mot.startsWith(s)) E.push(mot);
-  });
-  return E;
-};
-
-/**
- * @param {String} s le mot fini par s
- * @param {String[]} Mots Liste des mots a filtrer
- */
-const end = (s, M) => {
-  var E = [];
-  M.forEach((mot) => {
-    if (mot.endsWith(s)) E.push(mot);
-  });
-  return E;
-};
-/**
- *
- * @param {Number} n
- * @param {String[]} M
- */
-const sized = (n, M) => {
-  var E = [];
-  M.forEach((mot) => {
-    if (mot.length === n) E.push(mot);
-  });
-  return E;
-};
-
-/**
- * @param {String} s suite de caractère dans la liste de mot
- * @param {String[]} M liste de mot
- */
-const suite = (s, M) => {
-  var E = [];
-  M.forEach((mot) => {
-    if (mot.includes(s)) E.push(mot);
-  });
-  return E;
-};
+const axios = require('axios').default;
 
 export default {
   data() {
@@ -203,32 +77,34 @@ export default {
       var notinside = document.getElementById("notinclude").value.toLowerCase();
       var sizes = document.getElementById("selector").value.toLowerCase();
       var suites = document.getElementById("suites").value.toLowerCase();
-
-      var E = lesmots;
-      if (starts) {
-        E = start(starts, E || lesmots);
-      }
-      if (ends) {
-        E = end(ends, E || lesmots);
-      }
-      if (inside) {
-        E = contain(inside.split(""), E || lesmots);
-      }
-      if (notinside) {
-        E = nocontain(notinside.split(""), E || lesmots);
-      }
-      if (suites) {
-        E = suite(suites, E || lesmots);
-      }
-      if (sizes) {
-        E = sized(parseInt(sizes), E || lesmots);
-      }
-      var resp = document.getElementById("resp");
-      resp.innerText = E.join(", ");
-      var counter = document.getElementById("counter");
-      counter.innerText = `${E.length} / ${lesmots.length} = ${
-        Math.round((E.length / lesmots.length) * 1000000) / 10000
-      }%`;
+      var url = "https://motsavecapi.herokuapp.com/";
+      var params = {
+        start: starts == undefined ? "" : starts,
+        end: ends == undefined ? "" : ends,
+        in: inside == undefined ? "" : inside,
+        nin: notinside == undefined ? "" : notinside,
+        size: sizes == undefined ? "" : sizes,
+        suite: suites == undefined ? "" : suites,
+      };
+      Object.keys(params).forEach(function(key) {
+        if (params[key] === "") {
+          delete params[key];
+        }
+      });
+      axios
+        .get(url, {
+          params: params,
+        })
+        .then((response) => {
+          var resp =  document.getElementById("resp");
+          resp.innerText = response.data.join(", ");
+          var counter = document.getElementById("counter");
+          counter.innerText = `${response.data.length} / 336531 = ${Math.round((response.data.length / 336531) * 1000000)/10000}%`;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      
     },
     reset() {
       document.getElementById("startby").value = "";
